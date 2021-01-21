c8e69f858bc436068a4516f272dc0bb31bba629f
Matt Kufchak <matt.kufchak@gmail.com>
2021-01-21 17:42:51 -0600

    finished login route

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
-router.post('/test', function(req, res) {
-  res.json({ requestBody: req.body });
-});
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+
+router.use('/session', sessionRouter);
+router.use('/users', usersRouter);
+
+
 
 module.exports = router;
