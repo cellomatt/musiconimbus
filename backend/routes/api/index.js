d36bb983ccc32b26aa679d98d429f57060dc34fa
Matt Kufchak <matt.kufchak@gmail.com>
2021-01-22 00:11:16 -0600

    complete part 1

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -6,6 +6,8 @@ const usersRouter = require('./users.js');
 router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
 
-
+router.post('/test', function(req, res) {
+  res.json({ requestBody: req.body });
+});
 
 module.exports = router;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,9 +4,6 @@ const apiRouter = require('./api');
 const router = express.Router();
 router.use('/api', apiRouter);
 
-router.get('/hello/world', function(req, res) {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!');
-});
+
 
 module.exports = router;
