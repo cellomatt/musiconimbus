5213b21b763ed892160e7b0d1e27c3a62d74169c
Matt Kufchak <matt.kufchak@gmail.com>
2021-01-22 12:57:26 -0600

    finished frontend setup

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,8 +2,35 @@ const express = require('express');
 const apiRouter = require('./api');
 
 const router = express.Router();
+
+if (process.env.NODE_ENV !== 'production') {
+  router.get('/api/csrf/restore', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.json({});
+  });
+}
+
 router.use('/api', apiRouter);
 
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path');
+
+  router.get('/', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+    );
+  });
+
+  router.use(express.static(path.resolve("../frontend/build")));
+
+  router.get(/^(?!\/?api).*/, (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+    );
+  });
+}
 
 
 module.exports = router;
