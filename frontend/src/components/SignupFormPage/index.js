19deb8a76a6d5a1087ef176896b3d82ef450e8d5
Matt Kufchak <matt.kufchak@gmail.com>
2021-01-24 01:00:48 -0600

    added css file

--- frontend/src/components/SignupFormPage/index.js
+++ frontend/src/components/SignupFormPage/index.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
 import * as sessionActions from "../../store/session";
+import './SignupForm.css';
 
 export default function SignupFormPage() {
   const dispatch = useDispatch();
