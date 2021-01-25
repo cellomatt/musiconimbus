4c2b8dfe0b75a583e3ed98ad2fe7e8ca8c99a8a0
Matt Kufchak <matt.kufchak@gmail.com>
2021-01-25 00:39:31 -0600

    many more css fixes

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -7,8 +7,9 @@ export default function Home() {
     <div className="main">
       <h1>Make music.</h1>
       <h1>Share it with the world.</h1>
-      <p>MusicoNimbus is a place for classical musicians to upload, share, and listen to great music.</p>
+      <p className="tagline">MusicoNimbus is a place for classical musicians to upload, share, and listen to great music.</p>
       <img className="logo" src={logo} alt="clouds logo" />
+      <p className="disclaimer">The music on this website is made available under fair use for educational/demonstration purposes only. <br></br>It is not available for sale or reuse.</p>
     </div>
   );
 }
