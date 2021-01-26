19328231da9469c80bac5b53e367ceff1e665aae
Matt Kufchak <matt.kufchak@gmail.com>
2021-01-25 18:19:56 -0600

    added some seed data

--- backend/db/seeders/20210126000513-user-data.js
+++ backend/db/seeders/20210126000513-user-data.js
@@ -89,9 +89,6 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
-    const Op = Sequelize.Op;
-    return queryInterface.bulkDelete('Users', {
-      username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] }
-    }, {});
+    return queryInterface.bulkDelete('Users', null, {});
   }
 };
