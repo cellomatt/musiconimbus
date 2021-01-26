d8903621a77063d2bb39dd76d787c5d28259f2bb
Matt Kufchak <matt.kufchak@gmail.com>
2021-01-26 00:33:42 -0600

    more album seed data

--- backend/db/seeders/20210126031035-album-data.js
+++ backend/db/seeders/20210126031035-album-data.js
@@ -31,7 +31,34 @@ module.exports = {
           imageUrl: 'https://musiconimbus.s3.us-east-2.amazonaws.com/CordovaQuartet-cover.jpg',
           description: 'Featuring J.S. Bach\'s Partitas no. 2 and 3 and Sonata no. 3 for solo violin.'
         },
-
+        {
+          title: 'Shostakovich: The 15 String Quartets',
+          artistId: 6,
+          releaseDate: 2007,
+          imageUrl: 'https://musiconimbus.s3.us-east-2.amazonaws.com/CordovaQuartet-cover.jpg',
+          description: 'The complete string quartets of Dmitri Shostakovich, recorded by the ensemble for whom they were written between 1956-1974.'
+        },
+        {
+          title: 'Takacs Haydn',
+          artistId: 6,
+          releaseDate: 2007,
+          imageUrl: 'https://musiconimbus.s3.us-east-2.amazonaws.com/CordovaQuartet-cover.jpg',
+          description: 'The complete string quartets of Dmitri Shostakovich, recorded by the ensemble for whom they were written between 1956-1974.'
+        },
+        {
+          title: 'YoYo tangos',
+          artistId: 6,
+          releaseDate: 2007,
+          imageUrl: 'https://musiconimbus.s3.us-east-2.amazonaws.com/CordovaQuartet-cover.jpg',
+          description: 'The complete string quartets of Dmitri Shostakovich, recorded by the ensemble for whom they were written between 1956-1974.'
+        },
+        {
+          title: 'yoyo kodaly',
+          artistId: 6,
+          releaseDate: 2007,
+          imageUrl: 'https://musiconimbus.s3.us-east-2.amazonaws.com/CordovaQuartet-cover.jpg',
+          description: 'The complete string quartets of Dmitri Shostakovich, recorded by the ensemble for whom they were written between 1956-1974.'
+        },
     ], {});
   },
 
