3765deb7d4ff1ff17ad9e48e01cd3c30ac56dbba
Matt Kufchak <matt.kufchak@gmail.com>
2021-01-26 09:59:55 -0600

    completed album seed

--- backend/db/seeders/20210126031035-album-data.js
+++ backend/db/seeders/20210126031035-album-data.js
@@ -14,50 +14,50 @@ module.exports = {
           title: 'Romantic Cello Favorites',
           artistId: 5,
           releaseDate: 1989,
-          imageUrl: 'https://musiconimbus.s3.us-east-2.amazonaws.com/CordovaQuartet-cover.jpg',
+          imageUrl: 'https://musiconimbus.s3.us-east-2.amazonaws.com/starker.jpg',
           description: 'A tribute to cellist and composer David Popper.'
         },
         {
           title: 'Brahms',
           artistId: 7,
           releaseDate: 2007,
-          imageUrl: 'https://musiconimbus.s3.us-east-2.amazonaws.com/CordovaQuartet-cover.jpg',
+          imageUrl: 'https://musiconimbus.s3.us-east-2.amazonaws.com/emerson-cover.jpg',
           description: 'Featuring Brahms\' piano quintet with Leon Fleisher and all three string quartets.'
         },
         {
           title: 'Hilary Hahn plays Bach',
           artistId: 2,
           releaseDate: 1997,
-          imageUrl: 'https://musiconimbus.s3.us-east-2.amazonaws.com/CordovaQuartet-cover.jpg',
+          imageUrl: 'https://musiconimbus.s3.us-east-2.amazonaws.com/hilary-cover.jpg',
           description: 'Featuring J.S. Bach\'s Partitas no. 2 and 3 and Sonata no. 3 for solo violin.'
         },
         {
           title: 'Shostakovich: The 15 String Quartets',
           artistId: 6,
           releaseDate: 2007,
-          imageUrl: 'https://musiconimbus.s3.us-east-2.amazonaws.com/CordovaQuartet-cover.jpg',
+          imageUrl: 'https://musiconimbus.s3.us-east-2.amazonaws.com/shostakovich-cover.jpg',
           description: 'The complete string quartets of Dmitri Shostakovich, recorded by the ensemble for whom they were written between 1956-1974.'
         },
         {
-          title: 'Takacs Haydn',
-          artistId: 6,
-          releaseDate: 2007,
-          imageUrl: 'https://musiconimbus.s3.us-east-2.amazonaws.com/CordovaQuartet-cover.jpg',
-          description: 'The complete string quartets of Dmitri Shostakovich, recorded by the ensemble for whom they were written between 1956-1974.'
+          title: 'Haydn String Quartets, Op. 76 1-3',
+          artistId: 8,
+          releaseDate: 1988,
+          imageUrl: 'https://musiconimbus.s3.us-east-2.amazonaws.com/Takacs-Haydn.jpg',
+          description: null
         },
         {
-          title: 'YoYo tangos',
-          artistId: 6,
-          releaseDate: 2007,
-          imageUrl: 'https://musiconimbus.s3.us-east-2.amazonaws.com/CordovaQuartet-cover.jpg',
-          description: 'The complete string quartets of Dmitri Shostakovich, recorded by the ensemble for whom they were written between 1956-1974.'
+          title: 'Soul of the Tango',
+          artistId: 4,
+          releaseDate: 1997,
+          imageUrl: 'https://musiconimbus.s3.us-east-2.amazonaws.com/tango-cover.jpg',
+          description: 'The music of Astor Piazzolla.'
         },
         {
-          title: 'yoyo kodaly',
-          artistId: 6,
-          releaseDate: 2007,
-          imageUrl: 'https://musiconimbus.s3.us-east-2.amazonaws.com/CordovaQuartet-cover.jpg',
-          description: 'The complete string quartets of Dmitri Shostakovich, recorded by the ensemble for whom they were written between 1956-1974.'
+          title: 'Solo',
+          artistId: 4,
+          releaseDate: 1999,
+          imageUrl: 'https://musiconimbus.s3.us-east-2.amazonaws.com/kodaly-cover.jpg',
+          description: 'An exploration of solo music for the cello, influenced by cultures around the world.'
         },
     ], {});
   },
