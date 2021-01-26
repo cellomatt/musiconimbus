2a845349bd5ca351a5226801e995f0a4ee7a4dd4
Matt Kufchak <matt.kufchak@gmail.com>
2021-01-26 12:31:41 -0600

    updated lots of css

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -33,8 +33,8 @@ router.post(
   '',
   validateSignup,
   asyncHandler(async (req, res) => {
-    const { email, password, username } = req.body;
-    const user = await User.signup({ email, username, password });
+    const { email, password, username, firstName, lastName, artistName } = req.body;
+    const user = await User.signup({ email, username, password, firstName, lastName, artistName });
 
     await setTokenCookie(res, user);
 
