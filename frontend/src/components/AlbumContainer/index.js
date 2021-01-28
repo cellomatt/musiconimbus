6517e457d5a4d7629f8bfb929743d6c01d06723b
Matt Kufchak <matt.kufchak@gmail.com>
2021-01-27 18:41:57 -0600

    songs and composers render

--- frontend/src/components/AlbumContainer/index.js
+++ frontend/src/components/AlbumContainer/index.js
@@ -6,6 +6,7 @@ export default function AlbumContainer({ album }) {
     <Link to={`/albums/${album.id}`}>
       <div className="container" >
         <h2 className="album__title">{album.title}</h2>
+        <h3>{album.releaseDate}</h3>
         <img src={album.imageUrl} alt="album cover" className="album__cover"/>
       </div>
     </Link>
--- frontend/src/components/SongContainer/index.js
+++ frontend/src/components/SongContainer/index.js
@@ -5,8 +5,9 @@ export default function SongContainer( {song} ) {
 
   return (
       <div className="song-container" >
+        <i class="fas fa-play"></i>
         <h3 className="">{song.title}</h3>
-        <p>{song.Composer.name}</p>
+        <p>{song.Composer.firstName}{song.Composer.lastName}</p>
         {/* <img src={album.imageUrl} className="album__cover"/> */}
       </div>
   )
