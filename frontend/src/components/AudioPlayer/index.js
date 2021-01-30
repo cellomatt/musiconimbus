480d243a181e73f6944994ec04a53e89d29386a1
Matt Kufchak <matt.kufchak@gmail.com>
2021-01-30 12:51:08 -0600

    swapped audio player and started styling it

--- frontend/src/components/AudioPlayer/index.js
+++ frontend/src/components/AudioPlayer/index.js
@@ -1,14 +1,16 @@
-import ReactAudioPlayer from 'react-audio-player';
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
 import "./AudioPlayer.css"
 
-export default function AudioPlayer({ nowPlaying }) {
+export default function MyAudioPlayer({ nowPlaying }) {
 
   return (
     <div className="player__container">
-      <ReactAudioPlayer className="player"
+      <AudioPlayer className="player"
         src={nowPlaying.songUrl}
         autoPlay
         controls
+        controlsList="nodownload"
       />
     </div>
   )
