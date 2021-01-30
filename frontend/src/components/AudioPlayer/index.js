958eeabbf54c294ef8e32f61fc088b635e9484fa
Matt Kufchak <matt.kufchak@gmail.com>
2021-01-30 14:10:24 -0600

    styling audio player

--- frontend/src/components/AudioPlayer/index.js
+++ frontend/src/components/AudioPlayer/index.js
@@ -1,16 +1,40 @@
-import AudioPlayer from 'react-h5-audio-player';
+import AudioPlayer, { RHAP_UI }  from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
 import "./AudioPlayer.css"
 
 export default function MyAudioPlayer({ nowPlaying }) {
 
+  const playNext = (nowPlaying) => {
+
+  }
+
+
   return (
     <div className="player__container">
       <AudioPlayer className="player"
         src={nowPlaying.songUrl}
+        defaultCurrentTime="0:00" defaultDuration="0:00"
+        layout="horizontal-reverse"
+        customAdditionalControls={[]}
+        customControlsSection={[RHAP_UI.MAIN_CONTROLS]}
+        customProgressBarSection={
+          [
+            RHAP_UI.CURRENT_TIME,
+            <div>/</div>,
+            RHAP_UI.DURATION,
+            RHAP_UI.PROGRESS_BAR,
+            RHAP_UI.VOLUME,
+          ]
+        }
+        customVolumeControls={[]}
+        // showSkipControls={true}
+        showJumpControls={false}
         autoPlay
         controls
         controlsList="nodownload"
+        // onEnded={playNext}
+        // onClickNext={playNext}
+        // onClickPrevious={playPrevious}
       />
     </div>
   )
